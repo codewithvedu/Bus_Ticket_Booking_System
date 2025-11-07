96fad1758b5d6a92b8cb9990c591604bac7a25d2
codewithvedu <vedant.padave.cmaug25@gmail.com>
2025-11-07 22:53:50 +0530

    Updated API configuration in api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:1234", 
+  baseURL: "http://localhost:1234",//"https://5nwglhmc-1234.inc1.devtunnels.ms", 
   headers: {
     "Content-Type": "application/json",
   },
